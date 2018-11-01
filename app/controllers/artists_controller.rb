7dcb1c254f89bcd722f88b585ddefac5a5cc761b
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-01 21:33:59 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -13,7 +13,7 @@ class ArtistsController < ApplicationController
   end
 
   def create
-    @artist = Artist.new(artist_params)
+    @artist = Artist.new(params.require(:artist).permit(:name, :bio))
     @artist.save
     redirect_to artist_path(@artist)
   end
